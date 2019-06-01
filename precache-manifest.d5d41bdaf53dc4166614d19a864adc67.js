935fd86ab7d9307e9443daa6a15239781ff26b44
Mucyo Miller <mucyomiller@gmail.com>
2019-06-01 14:33:36 +0200

    Updates

---
+++ precache-manifest.d5d41bdaf53dc4166614d19a864adc67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8751aa52847930deb851341643299af4",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "5d6ecf9f8cb0a53b2a0f",
+    "url": "/portofolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portofolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5d6ecf9f8cb0a53b2a0f",
+    "url": "/portofolio/static/js/main.a0829452.chunk.js"
+  },
+  {
+    "revision": "8ebee44fab047a9cd28e",
+    "url": "/portofolio/static/js/runtime~main.64b239cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portofolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
